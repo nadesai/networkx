906d69f9e66ca6cccb926c69bc7993a60f5032df
Nikhil Desai <ndesai@iqt.org>
2014-06-19 15:41:54 -0700

    Basic Girvan-Newman implementation

--- networkx/algorithms/community/girvan_newman.py
+++ networkx/algorithms/community/girvan_newman.py
@@ -6,8 +6,21 @@
 #    BSD license.
 
 import networkx as nx
+from networkx.algorithms.connectivity import local_edge_connectivity
+
 __author__ = """\n""".join(['Nikhil Desai (nikhilarundesai@gmail.com)'])
-__all__ = ['girvan_newman']
+__all__ = ['girvan_newman_splits']
 
 def girvan_newman_splits(G):
-    pass
+    g = G.copy()
+    splits = []
+    while G.number_of_edges() > 0:
+        ebw = nx.edge_betweenness(G)
+        max_edge = max(ebw, key=ebw.get)
+        G.remove_edge(*max_edge)
+        if in_same_connected_component(G, *max_edge): 
+            splits.append(max_edge)
+    return splits
+
+def in_same_connected_component(G, u, v):
+    return local_edge_connectivity(G, u, v) > 0
