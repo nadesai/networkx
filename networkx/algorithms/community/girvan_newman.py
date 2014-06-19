57cb97dbd94c6555cd2b5074e0d67b8e08e16aa9
Nikhil Desai <ndesai@iqt.org>
2014-06-19 14:54:08 -0700

    Girvan-Newman stub

---
+++ networkx/algorithms/community/girvan_newman.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+#    Copyright (C) 2011 by 
+#    Conrad Lee <conradlee@gmail.com>
+#    Aric Hagberg <hagberg@lanl.gov>
+#    All rights reserved.
+#    BSD license.
+
+import networkx as nx
+__author__ = """\n""".join(['Nikhil Desai (nikhilarundesai@gmail.com)'])
+__all__ = ['girvan_newman']
+
+def girvan_newman_splits(G):
+    pass
